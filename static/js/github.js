6c626ff64a1a5d8ddf72bdd7daf55fcae6ec57f1
Fabien Bellanger <fabien.bellanger@apitic.com>
2020-12-09 14:01:08 +0100

    Amélioration page projets Github

--- static/js/github.js
+++ static/js/github.js
@@ -1,6 +1,14 @@
 $(function () {
+    const now = moment();
+
     $('.datetime').each(function () {
-        $(this).html(moment($(this).text()).format('YYYY-MM-DD HH:mm'));
+        $(this).html(moment($(this).text().trim()).format('YYYY-MM-DD HH:mm'));
+    });
+
+    $('.datetime-human').each(function () {
+        const datetime = moment($(this).text().trim());
+        const duration2 = moment.duration(datetime.diff(now)).humanize(true);
+        $(this).html(duration2);
     });
 
     $('#releases').DataTable({
@@ -8,7 +16,7 @@ $(function () {
         'order': [[3, 'desc']],
     });
 
-    const expiredAt = moment($("#cacheExpiredAt").text());
-    const duration = moment.duration(expiredAt.diff(moment())).humanize(true);
+    const expiredAt = moment($("#cacheExpiredAt").text().trim());
+    const duration = moment.duration(expiredAt.diff(now)).humanize(true);
     $('#cacheExpiredAt').html(duration);
 });
